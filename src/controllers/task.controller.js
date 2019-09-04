eacc6e05aeb6d09d3154d7d8e148941de8d06bc3
soulevans07 <szi.adam@simonyi.bme.hu>
2019-09-04 12:07:08 +0200

    finished tasks route

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,11 +1,52 @@
 import Task from '../models/task.model'
 
 const controller = {
-  getTaskList: async function(request, h) {
-    const task_list = await Task.find().exec();
-    return task_list;
+  list: async (req, h) => {
+    const task_list = await Task.find().exec()
+    return task_list
   },
 
+  create: async (req, h) => {
+    if(!req.payload.text){
+      return h.response({ error: 'Missing field!' }).code(400)
+    }
+
+    let task = new Task({
+      done: false,
+      text: req.payload.text
+    })
+
+    task.save()
+    
+    return h.response(task)
+  },
+
+  get: async (req, h) => {
+    const task = await Task.findById(req.params.id).exec()
+    if (!task) return h.response({ error: 'No task with id: ' + req.params.id }).code(400)
+
+    return h.response(task);
+  },
+
+  delete: async (req, h) => {
+    const task = await Task.findById(req.params.id).exec()
+    if (!task) return h.response({ error: 'No task with id: ' + req.params.id }).code(400)
+
+    await task.delete()
+    return h.response().code(200)
+  },
+
+  update: async (req, h) => {
+    const task = await Task.findById(req.params.id).exec()
+    if (!task) return h.response({ error: 'No task with id: ' + req.params.id }).code(400)
+
+    task.done = req.payload.done !== undefined ? req.payload.done : task.done
+    task.text = req.payload.text !== undefined ? req.payload.text : task.text
+
+    await task.save()
+
+    return h.response(task)
+  }
 }
 
 export default controller
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -5,7 +5,31 @@ const task_routes = []
 task_routes.push({
   method: 'GET', 
   path: '/api/tasks',
-  handler: taskController.getTaskList
+  handler: taskController.list
+})
+
+task_routes.push({
+  method: 'POST',
+  path: '/api/tasks/new',
+  handler: taskController.create
+})
+
+task_routes.push({
+  method: 'GET',
+  path: '/api/tasks/{id}',
+  handler: taskController.get
+})
+
+task_routes.push({
+  method: 'POST',
+  path: '/api/tasks/{id}/update',
+  handler: taskController.update
+})
+
+task_routes.push({
+  method: 'DELETE',
+  path: '/api/tasks/{id}',
+  handler: taskController.delete
 })
 
 export default task_routes
